db977e7f93675448c63107cfcc6958762f518d8a
Our Air Quality <info@ourairquality.org>
2019-03-31 22:25:18 +1100

    pbuf_alloc: guard esp-open-rtos specific code.

--- src/core/pbuf.c
+++ src/core/pbuf.c
@@ -284,9 +284,13 @@ pbuf_alloc(pbuf_layer layer, u16_t length, pbuf_type type)
       }
 
       /* If pbuf is to be allocated in RAM, allocate memory for it. */
+#ifdef ESP_OPEN_RTOS
       uint32_t malloc_mask = set_malloc_regions(MALLOC_MASK_DRAM);
+#endif
       p = (struct pbuf *)mem_malloc(alloc_len);
+#ifdef ESP_OPEN_RTOS
       set_malloc_regions(malloc_mask);
+#endif
       if (p == NULL) {
         return NULL;
       }
